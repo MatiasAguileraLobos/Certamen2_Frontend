f6c57d34a05e8b43935fd1c792dff63f25633315
Matias Aguilera <matias.aguileralobos@gmail.com>
2025-11-07 13:00:38 -0300

    primer commit base

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Button } from "primereact/button"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>
+        <Button label="Cuidado" severity="warning" rounded ></Button>
+      </h1>
+    </>
+  )
+}
+
+export default App
